7287d0a2e03db25d3b2af217519cb88e6afeedd8
illast <illast@ttu.ee>
2022-02-26 15:35:15 +0200

    button and player classes

--- core/src/com/mygdx/game/objects/Button.java
+++ core/src/com/mygdx/game/objects/Button.java
@@ -1,16 +1,10 @@
 package com.mygdx.game.objects;
 
 import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
-public class Button extends GameObject{
+public class Button extends GameObject {
 
     public Button(Texture texture, float x, float y, float width, float height) {
         super(texture, x, y, width, height);
-    }
-
-    @Override
-    public void draw(SpriteBatch batch) {
-        super.draw(batch);
     }
 }
