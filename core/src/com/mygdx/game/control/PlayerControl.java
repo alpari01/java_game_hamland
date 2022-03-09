2c7fee656ec08533cbcc3b72907b84c70e2aaea3
illast <illast@ttu.ee>
2022-03-09 16:35:09 +0200

    line comments

--- core/src/com/mygdx/game/control/PlayerControl.java
+++ core/src/com/mygdx/game/control/PlayerControl.java
@@ -18,43 +18,23 @@ public class PlayerControl {
      */
     public void handle() {
 
-        // UP + RIGHT
-        if ((Gdx.input.isKeyPressed(Input.Keys.W) && Gdx.input.isKeyPressed(Input.Keys.D)) || (Gdx.input.isKeyPressed(Input.Keys.UP) && Gdx.input.isKeyPressed(Input.Keys.RIGHT))) {
-            polygon.setPosition(polygon.getX() + 1, polygon.getY() + 1);
-        }
-
-        // DOWN + RIGHT
-        else if ((Gdx.input.isKeyPressed(Input.Keys.S) && Gdx.input.isKeyPressed(Input.Keys.D)) || (Gdx.input.isKeyPressed(Input.Keys.DOWN) && Gdx.input.isKeyPressed(Input.Keys.RIGHT))) {
-            polygon.setPosition(polygon.getX() + 1, polygon.getY() - 1);
-        }
-
-        // UP + LEFT
-        else if ((Gdx.input.isKeyPressed(Input.Keys.W) && Gdx.input.isKeyPressed(Input.Keys.A)) || (Gdx.input.isKeyPressed(Input.Keys.UP) && Gdx.input.isKeyPressed(Input.Keys.LEFT))) {
-            polygon.setPosition(polygon.getX() - 1, polygon.getY() + 1);
-        }
-
-        // DOWN + LEFT
-        else if ((Gdx.input.isKeyPressed(Input.Keys.S) && Gdx.input.isKeyPressed(Input.Keys.A)) || (Gdx.input.isKeyPressed(Input.Keys.DOWN) && Gdx.input.isKeyPressed(Input.Keys.LEFT))) {
-            polygon.setPosition(polygon.getX() - 1, polygon.getY() - 1);
-        }
-
         // RIGHT
-        else if (Gdx.input.isKeyPressed(Input.Keys.D) || Gdx.input.isKeyPressed(Input.Keys.RIGHT)) {
+        if (Gdx.input.isKeyPressed(Input.Keys.D) || Gdx.input.isKeyPressed(Input.Keys.RIGHT)) {
             polygon.setPosition(polygon.getX() + 1, polygon.getY());
         }
 
         // LEFT
-        else if (Gdx.input.isKeyPressed(Input.Keys.A) || Gdx.input.isKeyPressed(Input.Keys.LEFT)) {
+        if (Gdx.input.isKeyPressed(Input.Keys.A) || Gdx.input.isKeyPressed(Input.Keys.LEFT)) {
             polygon.setPosition(polygon.getX() - 1, polygon.getY());
         }
 
         // UP
-        else if (Gdx.input.isKeyPressed(Input.Keys.W) || Gdx.input.isKeyPressed(Input.Keys.UP)) {
+        if (Gdx.input.isKeyPressed(Input.Keys.W) || Gdx.input.isKeyPressed(Input.Keys.UP)) {
             polygon.setPosition(polygon.getX(), polygon.getY() + 1);
         }
 
         // DOWN
-        else if (Gdx.input.isKeyPressed(Input.Keys.S) || Gdx.input.isKeyPressed(Input.Keys.DOWN)) {
+        if (Gdx.input.isKeyPressed(Input.Keys.S) || Gdx.input.isKeyPressed(Input.Keys.DOWN)) {
             polygon.setPosition(polygon.getX(), polygon.getY() - 1);
         }
     }
