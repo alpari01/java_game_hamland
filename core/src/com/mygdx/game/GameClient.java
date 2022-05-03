b771ce79e131339934cdf5f869310afbb48c78df
Alan Parik <alpari@ttu.ee>
2022-05-03 20:24:11 +0300

    Menu music added.

--- core/src/com/mygdx/game/GameClient.java
+++ core/src/com/mygdx/game/GameClient.java
@@ -13,9 +13,39 @@ public class GameClient extends Game {
 	// Create new client object.
 	public KryoClient client = new KryoClient();
 
+	//Music
+	private static MusicMaster music;
+	private static boolean isMusicUp;
+	private static boolean isSoundUp;
+
 	@Override
 	public void create() {
+		music = new MusicMaster("music_main.mp3");
+		// Sound and music are on by default.
+		isMusicUp = true;
+		isSoundUp = true;
+
 		Screen menuScreen = new NicknameScreen(this);
 		setScreen(menuScreen);
+	}
+
+	public MusicMaster getMusic() {
+		return music;
+	}
+
+	public void setIsMusicUp(boolean state) {
+		isMusicUp = state;
+	}
+
+	public boolean getIsMusicUp() {
+		return isMusicUp;
+	}
+
+	public void setIsSoundUp(boolean state) {
+		isSoundUp = state;
+	}
+
+	public boolean getIsSoundUp() {
+		return isSoundUp;
 	}
 }
